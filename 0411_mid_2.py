2ad608e1570b1e85662ba0f71ebf5a4dd2422a68
CWNU-djc <100703668+CWNU-djc@users.noreply.github.com>
2022-04-11 10:55:53 +0900

    Create 0411_mid_2.py

---
+++ 0411_mid_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+def printMatrixTypes(A) :
+  #code here
+  pass
+
+A = np.array([[3, 0], 
+              [-1, 2],
+              [1, 1]])
+print("Matrix A = ", printMatrixTypes(A))
