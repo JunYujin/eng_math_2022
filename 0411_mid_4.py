f6b25675ea43871c0bffd87f820124288b0337eb
CWNU-djc <100703668+CWNU-djc@users.noreply.github.com>
2022-04-11 11:20:11 +0900

    Create 0411_mid_4.py

---
+++ 0411_mid_4.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+
+def pinrtLeibnizFormula():
+    n = int(input()) # nxn matrix
+    nf = math.factorial(n) # permutaions
+  
+    #code here    
+    
+pinrtLeibnizFormula()
